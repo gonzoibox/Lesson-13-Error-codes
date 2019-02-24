5601c1fc8125494c35b2a1610f90febb760ad40d
anton_davidovich <dvd14@ukr.net>
2019-02-24 21:41:41 +0200

    Done with respond  to users.js  :id(string in array)

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var arrOfStr = {
+  1: "Let it be",
+  2: "Just do it",
+  3: "Here we go"
+};
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function (req, res, next) {
+  var getId = req.params.id;
+  var getIdStr = arrOfStr[getId];
+
+  if (getId < 1 || getId > 3) {
+    res.render('users', {
+      respond: "Error!"
+    });
+  } else {
+    res.render('users', {
+      respond: getIdStr
+    });
+  }
 });
 
 module.exports = router;
